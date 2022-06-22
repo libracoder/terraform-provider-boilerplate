ab1bb2875be904e8f6e419b0b2ab8afa7fa42ecb
libracoder <libracoder@gmail.com>
2022-06-22 22:04:27 +0100

    enhance: add person resource

---
+++ resources/person/create.go
@@ -0,0 +1,20 @@
+package person
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceServerCreate(d *schema.ResourceData, m interface{}) error {
+	person := d.Get("person").(string)
+
+	d.SetId(person)
+
+	//// https://www.uuidtools.com/api/generate/v1/count/uuid_count
+	//resp, err := http.Get("https://www.uuidtools.com/api/generate/v1/count/" + uuidCount)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//defer resp.Body.Close()
+
+	return resourceServerRead(d, m)
+}
---
+++ resources/person/read.go
@@ -0,0 +1,7 @@
+package person
+
+import "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+
+func resourceServerRead(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
